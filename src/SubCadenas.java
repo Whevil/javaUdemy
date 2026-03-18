0680e683e31769a8ee4a0faae8c021df32183fc0
Whevill <diego.barrera-b@uniminuto.edu.co>
2026-03-18 18:35:52 -0500

    manejo de cadenas encontrar indices con lastindexOf  y indexOf

---
+++ src/SubCadenas.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author diego
+ */
+public class SubCadenas {
+    public static void main(String[] args) {
+        var cadena1 = "hola mundo amigos mios";
+        
+        System.out.println(cadena1); 
+       System.out.println("con sub cadena ==" + cadena1.substring(0, 10));
+    
+        System.out.println("indice 2  == " + cadena1.lastIndexOf("mundo"));
+        System.out.println("para cadena no encontrada  " + cadena1.indexOf("java"));
+        System.out.println("last index" + cadena1.lastIndexOf("mundo"));
+    }
+    
+    
+    
+}
