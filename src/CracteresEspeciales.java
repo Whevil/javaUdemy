bb536d18490103dd9f30ccfdd042465bcc8afe48
Whevil <diego.barrera-b@uniminuto.edu.co>
2026-05-18 21:30:30 -0500

    comentario desde apache

---
+++ src/CracteresEspeciales.java
@@ -0,0 +1,20 @@
+
+
+
+/**
+ *
+ * @author diego
+ */
+public class CracteresEspeciales {
+    public static void main(String[] args) {
+        //caracteres especiales
+        var cadena = "hola \n mundo";
+        System.out.println("cadne = "+cadena);
+        
+        
+        
+        
+        
+    }
+    
+}
